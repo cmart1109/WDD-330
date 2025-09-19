7dab4eeeb39f4463e3a04e75b6ca6e30a00e9bdb
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-09-18 22:32:23 -0600

    team activity 2

--- src/js/product.js
+++ src/js/product.js
@@ -1,24 +1,9 @@
-import { getLocalStorage, setLocalStorage, getParam } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";; 
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./productDetails.mjs";
 
 const dataSource = new ProductData("tents");
-const productId = getParam("product");
+const productID = getParam("product");
 
-function addProductToCart(product) {
-  const cart = getLocalStorage("so-cart") || [];
-  cart.push(product);
-  setLocalStorage("so-cart", cart);
-}
-
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
-
-// add listener to Add to Cart button
-document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
-  console.log(dataSource.findProductById(productId));
-
+const product = new ProductDetails(productID, dataSource);
+product.init();
