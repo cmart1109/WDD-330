7c1b74c632d837423a15736e092b4801df0f78f6
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-09-20 15:17:18 -0600

    finish assignment2

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,8 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./productList.mjs";
 
-const dataSource = new ProductData('tents');
-const element = document.querySelector('.product-list');
+const dataSource = new ProductData("tents");
+const element = document.querySelector(".product-list");
 
-const productList = new ProductList('tents', dataSource, element);
-
+const productList = new ProductList("tents", dataSource, element);
 const list = new ProductList(ProductData);
