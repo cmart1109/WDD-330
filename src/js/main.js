05cff1a462ae5c541d448030432e6188d4d6711a
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-09-20 15:15:42 -0600

    commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,9 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./productList.mjs";
 
+const dataSource = new ProductData('tents');
+const element = document.querySelector('.product-list');
+
+const productList = new ProductList('tents', dataSource, element);
+
 const list = new ProductList(ProductData);
