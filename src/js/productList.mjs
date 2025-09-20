8ed793feeda286b393033cc0d1defcad19ee84cb
Christian Martinez <140223696+cmart1109@users.noreply.github.com>
2025-09-20 15:15:42 -0600

    commit

--- src/js/productList.mjs
+++ src/js/productList.mjs
@@ -1,13 +1,16 @@
+import { renderListWithTemplate } from './utils.mjs';
 
-export function productCardTemplate() {
-    return `<li class="product-card">
-    <a href="product_pages/?product=">
-      <img src="" alt="Image of ">
-      <h2 class="card__brand"></h2>
-      <h3 class="card__name"></h3>
-      <p class="product-card__price">$</p>
+
+
+function productCardTemplate(product) {
+  return `<li class="product-card">
+    <a href="product_pages/?product=${product.Id}">
+      <img src="${product.Image}" alt="Image of ${product.Name}">
+      <h2 class="card__brand">${product.Brand}</h2>
+      <h3 class="card__name">${product.Name}</h3>
+      <p class="product-card__price">$${product.Price}</p>
     </a>
-  </li>`
+  </li>`;
 }
 
 export default class ProductList {
@@ -16,7 +19,13 @@ export default class ProductList {
         this.dataSource = dataSource;
         this.listElement = listElement;
     }
+
     async init() {
-        const list = await this.dataSource.getData();
+      const list = await this.dataSource.getData();
+      this.renderList(list);
     }
-    }
+    renderList(list) {
+    renderListWithTemplate(productCardTemplate, this.listElement, list);
+    
+  };
+}
